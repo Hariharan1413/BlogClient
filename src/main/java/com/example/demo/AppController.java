0fb0497e3efd34baa042d1e8dfad2f92c7827927
ezhilcharu <ezhilcharu@gmail.com>
2022-11-22 15:34:10 +0530

    autowired repos in appController

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,8 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
+	@Autowired
+	BlogRepository blogRepo;
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	@Autowired
+	CommentRepository comRepo;
+	
+	@Autowired
+	CategoryRepository catRepo;
+	
+	@Autowired
+	FileDBRepository fileDbRepo;
 }
